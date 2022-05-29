3af8dac0d243eafe23654e836a66842e8454a6c3
Carlos Junior <carlos-junior08@hotmail.com>
2022-05-28 22:42:23 -0300

    Done

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
@@ -9,7 +9,19 @@ class TurnUserAdminUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error("Mensagem do erro");
+    }
+
+    Object.assign(user, {
+      ...user,
+      admin: true,
+      updated_at: new Date(),
+    })
+
+    return user;
   }
 }
 
